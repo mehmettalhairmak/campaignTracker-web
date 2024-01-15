8898662c695435684163a539cb118260ae51a5a2
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2024-01-16 00:28:05 +0300

    added global css and tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"base-white": "#fff",
+				"primary-primary-default": "#14b1b9",
+				gray: "#101828",
+				whitesmoke: "#eceeeb",
+			},
+			spacing: {},
+			fontFamily: {
+				alata: "Alata",
+				"inter-tight": "'Inter Tight'",
+			},
+		},
+		fontSize: {
+			xl: "1.25rem",
+			inherit: "inherit",
+		},
+	},
+	corePlugins: {
+		preflight: false,
+	},
+};
+export default config;
