27fa9bc414e5dbad7e16d2547d9137cd8e0180b1
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2024-01-16 12:31:46 +0300

    rechanged name

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<>
+			<div className="flex items-center justify-center h-[6.87rem]">
+				<div className="tracking-[-0.04em] font-alata text-[2.25rem] text-gray">
+					makromusic Task
+				</div>
+			</div>
+			<div className="w-full h-[0.06rem] border-t-[1px] border-solid border-whitesmoke" />
+		</>
+	);
+}
