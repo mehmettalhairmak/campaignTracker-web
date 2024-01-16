2670f4573dd0e290adb4c26d94909ff457493094
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2024-01-16 14:08:38 +0300

    create-campaign-header maked for dynamic

--- src/components/create-campaign/header.tsx
+++ src/components/create-campaign/header.tsx
@@ -1,26 +1,53 @@
-import React from "react";
+"use client";
 
-function CreateCampaignHeader() {
+import React, { ReactNode, useEffect } from "react";
+
+function CreateCampaignHeader({
+	currentPage,
+}: {
+	currentPage: "Parçan" | "Detaylar" | "Kampanyan" | "Ödeme";
+}) {
+	const renderSection = (): ReactNode => {
+		const pages = ["Parçan", "Detaylar", "Kampanyan", "Ödeme"];
+
+		return pages.map((value, index, array) => {
+			let spacing;
+
+			if (currentPage === value) {
+				spacing = "[0.5rem]";
+			} else if (pages.indexOf(currentPage) > index) {
+				spacing = "[2rem]";
+			} else {
+				spacing = "[0rem]";
+			}
+
+			return (
+				<React.Fragment key={index}>
+					<div className="text-center text-[1.25rem] text-gray-400">
+						{value}
+					</div>
+					{index < array.length - 1 && (
+						<div className="rounded-2xl bg-surface-surface-secondary w-[2rem] h-[0.25rem] overflow-hidden shrink-0 flex flex-row items-start justify-start">
+							<div
+								className={`self-stretch rounded-2xl bg-primary-600 w-${spacing} overflow-hidden shrink-0`}
+							/>
+						</div>
+					)}
+				</React.Fragment>
+			);
+		});
+	};
+
 	return (
 		<div className="w-full h-[6.87rem] flex flex-col">
 			<div className="flex flex-1 flex-row">
-				<div className="flex w-1/3 items-center justify-center gap-[0.44rem] text-center text-[1.25rem]">
-					<div className="tracking-[-0.04em]">Kampanya Oluştur</div>
+				<div className="flex w-1/3 items-center justify-center gap-[0.44rem]">
+					<div className="tracking-[-0.04em] text-center text-[1.25rem] text-gray-400">
+						Kampanya Oluştur
+					</div>
 				</div>
-				<div className="flex w-1/3 flex-row items-center justify-center gap-[1rem] text-gray-400">
-					<div className="tracking-[-0.04em] leading-[1.5rem]">Parçan</div>
-					<div className="rounded-2xl bg-surface-surface-secondary w-[2rem] h-[0.25rem] overflow-hidden shrink-0 flex flex-row items-start justify-start">
-						<div className="self-stretch rounded-2xl bg-primary-600 w-[0.5rem] overflow-hidden shrink-0" />
-					</div>
-					<div className="tracking-[-0.04em] leading-[1.5rem]">Detaylar</div>
-					<div className="rounded-2xl bg-surface-surface-secondary w-[2rem] h-[0.25rem] overflow-hidden shrink-0 flex flex-row items-start justify-start">
-						<div className="self-stretch bg-primary-600 w-[0rem] overflow-hidden shrink-0" />
-					</div>
-					<div className="tracking-[-0.04em] leading-[1.5rem]">Kampanyan</div>
-					<div className="rounded-2xl bg-surface-surface-secondary w-[2rem] h-[0.25rem] overflow-hidden shrink-0 flex flex-row items-start justify-start">
-						<div className="self-stretch bg-primary-600 w-[0rem] overflow-hidden shrink-0" />
-					</div>
-					<div className="tracking-[-0.04em] leading-[1.5rem]">Ödeme</div>
+				<div className="flex w-1/3 flex-row items-center justify-center gap-[1rem]">
+					{renderSection()}
 				</div>
 				<div className="w-1/3" />
 			</div>
